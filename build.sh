927788559fd4656054fe9ab6d2e76cff6cc25793
Philipp Rothenhäusler <phirot@kth.se>
2021-03-13 18:07:21 +0100

    Add mkdir to build script
    Add test *.kcd to .gitignore, remove tracking of test.kcd

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+#
+#
+# Author: Philipp Rothenhäusler, Stockholm 2020
+#
 
-APP_DIR=/home/flip/workspaces/sml/TestActivity/
+APP_DIR=/home/flip/workspaces/sml/rcve_ui
 
 cwd="$(readlink -m "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )")"
 
@@ -13,5 +17,6 @@ echo "Building CANBabel.jar ..."
 cd $cwd/external/canbabel && mvn clean install
 echo "[x] Maven installation has been found."
 echo "Installing CANBabel.jar ..."
+mkdir -p ${APP_DIR}/app/libs
 mv target/CANBabel-2.1-jar-with-dependencies.jar ${APP_DIR}/app/libs/CANBabel.jar
 echo "[x] Maven installation successfull. Ensure that dependencies to app/libs have been defined."
